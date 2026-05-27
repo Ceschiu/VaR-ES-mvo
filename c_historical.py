ba264e087a9dca77167a8623a2609fad4fcaf972
Ceschiu <xx99bionicoxx@gmail.com>
2026-05-27 15:21:10 +0200

    add code to c_historical

--- c_historical.py
+++ c_historical.py
@@ -8,10 +8,8 @@ def VaR_Historical(w:pd.Series, rend:pd.DataFrame, alpha:float = 0.95) -> tuple[
 
     # Calcolo VaR e ES del ptf con confidenza alpha
     VaR = -rend_ptf.quantile(1-alpha)
+    ES = -rend_ptf[rend_ptf< -VaR].mean()
     
-    rend_ptf_ES = rend_ptf[rend_ptf< -VaR]
-    ES = -rend_ptf_ES.mean()
-
     # Metodo meno veloce
     # rend_ptf = np.sort_values(rend_ptf)
     # n = int(np.floor((1-alpha)*len(rend_ptf)))
